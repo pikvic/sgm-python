9ff2dd5218ce33c2763743265eec1a579d843700
Alexey Zagumennov <truepikvic@gmail.com>
2020-05-05 13:10:14 +1000

    Added basic API, data.csv and creating uploads dir

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 from flask import Flask, jsonify, request
+from pathlib import Path
 import os
 
 
 app = Flask(__name__)
+root = Path()
+uploads = root / 'uploads'
+if not uploads.exists():
+    uploads.mkdir()
 
 
 @app.route('/')
 def index():
     return "Hello, World!"
 
+
+@app.route('/test')
+def test():
+    return jsonify({'success': True, 'result': 'Test Data'})
+
+
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return jsonify({'success': False, 'error': 'Not found'}), 404
 
 
 if __name__ == '__main__':
