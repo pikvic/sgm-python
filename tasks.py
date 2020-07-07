74d1a96bc8d1d8b0615cca44ed99cd408f0bc896
Alexey Zagumennov <truepikvic@gmail.com>
2020-07-07 19:51:22 +1000

    Fix logging

--- tasks.py
+++ tasks.py
@@ -16,10 +16,15 @@ import config
 
 import logging
 import sys
-from logging.handlers import TimedRotatingFileHandler
+from logging.handlers import TimedRotatingFileHandler, 
 
 FORMATTER = logging.Formatter("%(asctime)s — %(name)s — %(levelname)s — %(message)s")
 LOG_FILE = "my_app.log"
+
+def get_stream_handler():
+   stream_handler = logging.StreamHandler(sys.stdout)
+   stream_handler.setFormatter(FORMATTER)
+   return stream_handler
 
 def get_file_handler():
    file_handler = TimedRotatingFileHandler(LOG_FILE, when='midnight')
@@ -30,6 +35,7 @@ def get_logger(logger_name):
    logger = logging.getLogger(logger_name)
    logger.setLevel(logging.DEBUG) # better to have too much log than not enough
    logger.addHandler(get_file_handler())
+   logger.addHandler(get_stream_handler())
    # with this pattern, it's rarely necessary to propagate the error up to parent
    logger.propagate = False
    return logger
